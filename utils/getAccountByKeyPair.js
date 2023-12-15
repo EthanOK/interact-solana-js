81b9d834631981123d580e8b885373626a6b527d
EthanOK <sdzdj01@163.com>
2023-12-15 09:24:08 +0800

    new file:   utils/parsePair.js

--- utils/getAccountByKeyPair.js
+++ utils/getAccountByKeyPair.js
@@ -3,11 +3,10 @@ const { Keypair } = SOLANA;
 const { getPrivateKeyBySecretKey } = require("./getPrivateKeyBySecretKey");
 const pair0 = require("../keyPairs/AQAMLqdN3LSvaHx5tCVeWZWDRTGqL7QuvNgojCb3pS6Z.json");
 const { SOLANA_KEYPAIR } = require("./config");
+const { parsePair } = require("./parsePair");
 
 async function main() {
-  console.log(typeof SOLANA_KEYPAIR);
-  console.log(typeof pair0);
-  const keyPair = Keypair.fromSecretKey(Uint8Array.from(pair0));
+  const keyPair = parsePair(SOLANA_KEYPAIR);
 
   console.log("publicKey:" + keyPair.publicKey.toString());
 
---
+++ utils/parsePair.js
@@ -0,0 +1,9 @@
+const { Keypair } = require("@solana/web3.js");
+
+function parsePair(str_keyPair) {
+  const pair = JSON.parse(str_keyPair);
+  const keyPair = Keypair.fromSecretKey(Uint8Array.from(pair));
+
+  return keyPair;
+}
+module.exports = { parsePair };
