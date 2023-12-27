007ea9d492a455fc8424328c61b6b2259df42fc9
EthanOK <sdzdj01@163.com>
2023-12-27 16:19:06 +0800

    modified:   utils/transferFTokenTo.js

--- scripts/transferFToken.js
+++ scripts/transferFToken.js
@@ -6,22 +6,22 @@ const {
 } = require("../utils/mintTokenTo");
 const { parsePair } = require("../utils/parsePair");
 const { getTokenDecimals } = require("../utils/tokenInfo");
-const { transferERC20Token } = require("../utils/transferFTokenTo");
+const { transferERC20Token, transferERC20TokenIfNotExistCreateToAT } = require("../utils/transferFTokenTo");
 
 const connection = getDevConnection();
 const signer = parsePair(SOLANA_KEYPAIR);
-const tokenAddress = "Dmi5tZumaHP5qqh6196x715J2yiEHSS5Zx2rVcz3LnwP";
+const tokenAddress = "2UdeCFZ38Y529jp4GL1AQsJpQLJdLKyW9hNk8H3SxD9B";
 const receiverAddress = "2xuEyZoSkiiNBAgL21XobUCraojPUZ82GHuWpCPgpyXF";
 async function main() {
   const decimals = await getTokenDecimals(connection, tokenAddress);
 
-  await transferERC20Token(
+  await transferERC20TokenIfNotExistCreateToAT(
     connection,
     signer,
     tokenAddress,
     receiverAddress,
     100 * 10 ** decimals
   );
-  
+
 }
 main().then(() => process.exit(0));
--- utils/transferFTokenTo.js
+++ utils/transferFTokenTo.js
@@ -5,6 +5,7 @@ const {
   createAssociatedTokenAccountInstruction,
   ASSOCIATED_TOKEN_PROGRAM_ID,
   transfer,
+  createTransferInstruction,
 } = require("@solana/spl-token");
 const { PublicKey } = require("./config");
 const { getNativeBalance } = require("./getNativeBalance");
@@ -63,14 +64,91 @@ async function transferERC20Token(
       new PublicKey(toAssociatedTokenAddress),
       signer.publicKey,
       amount,
+      [signer],
+      "confirmed"
+    );
+    // 'confirmed' | 'finalized'
+    console.log("signature:" + signature);
+  } else {
+    console.log(toAssociatedTokenAddress, "Account does not exist");
+  }
+}
+
+async function transferERC20TokenIfNotExistCreateToAT(
+  connection,
+  signer,
+  tokenAddress,
+  receiverAddress,
+  amount
+) {
+  const signerBalance = await getTokenBalance(
+    connection,
+    tokenAddress,
+    signer.publicKey
+  );
+  if (signerBalance < amount) {
+    console.log("余额不足");
+    return;
+  }
+  const fromAssociatedTokenAddress = await getAssociatedTokenAccountAddress(
+    tokenAddress,
+    signer.publicKey
+  );
+
+  const toAssociatedTokenAddress = await getAssociatedTokenAccountAddress(
+    tokenAddress,
+    receiverAddress
+  );
+  const isCreate = await tokenAccountIsCreated(
+    connection,
+    toAssociatedTokenAddress
+  );
+
+  const balance = await getNativeBalance(connection, toAssociatedTokenAddress);
+
+  if (isCreate || balance > 0) {
+    const signature = await transfer(
+      connection,
+      signer,
+      new PublicKey(fromAssociatedTokenAddress),
+      new PublicKey(toAssociatedTokenAddress),
+      signer.publicKey,
+      amount,
       [],
       "confirmed"
     );
     // 'confirmed' | 'finalized'
     console.log("signature:" + signature);
   } else {
-    console.log(associatedTokenAddress, "Account does not exist");
+    const [ownerPublicKey, signers] = getSigners(signer.publicKey, []);
+    // 两条指令 1: reateAssociatedTokenAccount 2: createTransfer
+    const transaction = new Transaction().add(
+      createAssociatedTokenAccountInstruction(
+        signer.publicKey,
+        new PublicKey(toAssociatedTokenAddress),
+        new PublicKey(receiverAddress),
+        new PublicKey(tokenAddress),
+        TOKEN_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID
+      ),
+      createTransferInstruction(
+        new PublicKey(fromAssociatedTokenAddress),
+        new PublicKey(toAssociatedTokenAddress),
+        ownerPublicKey,
+        amount,
+        [],
+        TOKEN_PROGRAM_ID
+      )
+    );
+
+    const signature = await sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [signer, ...signers],
+      "confirmed"
+    );
+    console.log("signature:" + signature);
   }
 }
 
-module.exports = { transferERC20Token };
+module.exports = { transferERC20Token, transferERC20TokenIfNotExistCreateToAT };
